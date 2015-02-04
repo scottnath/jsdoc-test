ce94a5004e95dbf7f3c78522319d19169e630ef8
Scott Nath <github@scottnath.com>
2015-02-04 16:57:53 -0500

    initial commit

---
+++ gulp/tasks/development/browser-sync.js
@@ -0,0 +1,30 @@
+/**
+ *  @fileOverview Uses Gulpjs to trigger browser-sync
+ *
+ *  @author       Scott Nath
+ *
+ *  @requires     NPM:gulp
+ *  @requires     NPM:browser-sync
+ *  @requires     /gulp/config.js
+ */
+'use strict';
+var gulp        = require('gulp');
+var browsersync = require('browser-sync');
+var config      = require('../../config');
+
+/**
+ * Gulp task that triggers browser-sync
+ * @method browsersync
+ *
+ * @param   config.browsersync.development development configuration
+ *
+ */
+gulp.task('browsersync', function() {
+  browsersync.init(null, config.browsersync.development, function (err, bs) {
+    console.log('Started connect web server on ' + config.browsersync.development.host + ':' + config.browsersync.development.port);
+  });;
+});
+
+gulp.task('browsersync-reload', function(){
+  browsersync.reload();
+});
---
+++ gulp/tasks/test.js
@@ -0,0 +1,10 @@
+/**
+ * A Test function I put in here
+ * @function testfunction
+ *
+ * @param   {{string}}  foo foo is a thing
+ *
+ */
+function testfunction(foo){
+  foo = bar;
+}
